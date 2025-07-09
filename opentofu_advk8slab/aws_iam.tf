b22dcf4ff2c0dbd18949f66c52249c29406cb526
miroslavG-mg <mgospodinov@mail.bg>
2025-07-09 08:16:38 +0300

    Add iam-policies

--- opentofu_advk8slab/aws_iam.tf
+++ opentofu_advk8slab/aws_iam.tf
@@ -2,76 +2,7 @@ resource "aws_iam_policy" "master-policy" {
   name        = "k8slab-iam-master-policy"
   description = "iam-master-policy"
 
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "autoscaling:DescribeAutoScalingGroups",
-          "autoscaling:DescribeLaunchConfigurations",
-          "autoscaling:DescribeTags",
-          "ec2:DescribeInstances",
-          "ec2:DescribeRegions",
-          "ec2:DescribeRouteTables",
-          "ec2:DescribeSecurityGroups",
-          "ec2:DescribeSubnets",
-          "ec2:DescribeVolumes",
-          "ec2:DescribeAvailabilityZones",
-          "ec2:CreateSecurityGroup",
-          "ec2:CreateTags",
-          "ec2:CreateVolume",
-          "ec2:ModifyInstanceAttribute",
-          "ec2:ModifyVolume",
-          "ec2:AttachVolume",
-          "ec2:AuthorizeSecurityGroupIngress",
-          "ec2:CreateRoute",
-          "ec2:DeleteRoute",
-          "ec2:DeleteSecurityGroup",
-          "ec2:DeleteVolume",
-          "ec2:DetachVolume",
-          "ec2:RevokeSecurityGroupIngress",
-          "ec2:DescribeVpcs",
-          "ec2:DescribeInstanceTopology",
-          "elasticloadbalancing:AddTags",
-          "elasticloadbalancing:AttachLoadBalancerToSubnets",
-          "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
-          "elasticloadbalancing:CreateLoadBalancer",
-          "elasticloadbalancing:CreateLoadBalancerPolicy",
-          "elasticloadbalancing:CreateLoadBalancerListeners",
-          "elasticloadbalancing:ConfigureHealthCheck",
-          "elasticloadbalancing:DeleteLoadBalancer",
-          "elasticloadbalancing:DeleteLoadBalancerListeners",
-          "elasticloadbalancing:DescribeLoadBalancers",
-          "elasticloadbalancing:DescribeLoadBalancerAttributes",
-          "elasticloadbalancing:DetachLoadBalancerFromSubnets",
-          "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-          "elasticloadbalancing:ModifyLoadBalancerAttributes",
-          "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-          "elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer",
-          "elasticloadbalancing:AddTags",
-          "elasticloadbalancing:CreateListener",
-          "elasticloadbalancing:CreateTargetGroup",
-          "elasticloadbalancing:DeleteListener",
-          "elasticloadbalancing:DeleteTargetGroup",
-          "elasticloadbalancing:DescribeListeners",
-          "elasticloadbalancing:DescribeLoadBalancerPolicies",
-          "elasticloadbalancing:DescribeTargetGroups",
-          "elasticloadbalancing:DescribeTargetHealth",
-          "elasticloadbalancing:ModifyListener",
-          "elasticloadbalancing:ModifyTargetGroup",
-          "elasticloadbalancing:RegisterTargets",
-          "elasticloadbalancing:DeregisterTargets",
-          "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
-          "iam:CreateServiceLinkedRole",
-          "kms:DescribeKey"
-        ],
-        "Resource": [
-          "*"
-        ]
-      }
-  ]
-  })
+  policy = file("iam-policies/master-policy.json")
 }
 
 resource "aws_iam_role" "k8slab-master-role" {
@@ -106,26 +37,7 @@ resource "aws_iam_policy" "worker-policy" {
   name        = "k8slab-iam-worker-policy"
   description = "iam-worker-policy"
 
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "ec2:DescribeInstances",
-          "ec2:DescribeRegions",
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:GetRepositoryPolicy",
-          "ecr:DescribeRepositories",
-          "ecr:ListImages",
-          "ecr:BatchGetImage"
-        ],
-        "Resource": "*"
-      }
-  ]
-  })
+  policy = file("iam-policies/worker-policy.json")
 }
 
 resource "aws_iam_role" "k8slab-worker-role" {
